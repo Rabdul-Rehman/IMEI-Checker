61f22376e3c9b4dc98f6364f1d37f86f644da77d
abdulrehman990 <mabdulf64@gmail.com>
2026-07-30 22:04:15 +0500

    Initial commit: IMEI.info Next.js project

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,6 @@ const oxanium = Oxanium({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
-
 export const metadata = {
   title: "IMEI Info Replica",
   description: "Check phone details by IMEI number",
