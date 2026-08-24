190476040fbbd56a50ffc920d281c47db366112e
rabdul-26 <rabdulrehman2674@gmail.com>
2026-08-24 16:15:09 +0500

    Update IMEI API and add Supabase project configuration

--- api/server.js
+++ api/server.js
@@ -91,30 +91,25 @@ async function start() {
     */
 
     await fastify.register(cors, {
-  origin: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+      origin: [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+      ],
 
-    // Cloudflare frontend
-    "https://hist-bugs-dna-america.trycloudflare.com",
-  ],
+      methods: [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+        "OPTIONS",
+      ],
 
-  methods: [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-  ],
-
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-  ],
-
-  credentials: true,
-});
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+      ],
+    });
 
     /*
     =================================================
